3b57fd65090579fb38841c672e16c40086384fd8
Luis Aguilar <149074526+LuisAg1256@users.noreply.github.com>
2025-11-14 10:53:15 -0500

    mejoras

--- TallerSemana5/turismoComunidad/js/script.js
+++ TallerSemana5/turismoComunidad/js/script.js
@@ -1,13 +1,27 @@
-jQuery(document).ready(function($){
-    jQuery(document).scroll(function(){
-        if($(jQuery(document).scroll).scrollTop()>650){
+// CAMBIO DE NAV CON SCROLL
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 250) {
             $('#menuPrincipal').addClass('bgSiteMenu');
-            $("#logotipo").addClass("logotipo1");
-            $("#logotipo").removeClass("logotipo2");
-        }else{
-            $("#menuPrincipal").removeClass("bgSiteMenu");
-            $("#logotipo").removeClass("logotipo1");
-            $("#logotipo").addClass("logotipo2");
+            $("#logotipo").addClass("logotipo1").removeClass("logotipo2");
+        } else {
+            $('#menuPrincipal').removeClass('bgSiteMenu');
+            $("#logotipo").removeClass("logotipo1").addClass("logotipo2");
         }
     });
+
+    // BOTÓN VOLVER ARRIBA
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $("#btnTop").fadeIn();
+        } else {
+            $("#btnTop").fadeOut();
+        }
+    });
+
+    $("#btnTop").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+    });
+
 });
