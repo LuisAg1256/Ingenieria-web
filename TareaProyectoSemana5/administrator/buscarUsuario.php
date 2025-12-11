d1e76e0493337a10b2fd36879999df798e84d736
JU4NSCV <149074526+LuisAg1256@users.noreply.github.com>
2025-12-11 09:14:28 -0500

    Update buscarUsuario.php

--- TareaProyectoSemana5/administrator/buscarUsuario.php
+++ TareaProyectoSemana5/administrator/buscarUsuario.php
@@ -14,14 +14,13 @@ $id = intval($_GET["id"]);
 $miconexion = new clase_mysqli;
 $miconexion->conectar(DBHOST, DBUSER, DBPASS, DBNAME);
 
-// OJO: tu clase devuelve lista con índices como en validar.php
 $miconexion->consulta("SELECT * FROM usuarios WHERE id = $id");
 $usuario = $miconexion->consulta_lista();
 
-if (!$usuario || !$usuario[0]) {
-    echo json_encode(["ok" => false, "msg" => "Usuario no encontrado"]);
-    exit;
-}
+    if (!$usuario || !$usuario[0]) {
+        echo json_encode(["ok" => false, "msg" => "Usuario no encontrado"]);
+        exit;
+    }
 
 // $usuario[0] = id
 // $usuario[1] = nombre
